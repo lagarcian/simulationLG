1a1838207c73e6259e04d612c9b649ae8da9fced
lagarcian <53913517+lagarcian@users.noreply.github.com>
2019-08-31 08:27:19 -0500

    HW5
    Solución

---
+++ HW5.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+import numpy as np
+import math as m
+
+X=3192408188
+
+Y=X/(np.power(10,9))
+Y=m.floor(Y)
+print(Y)
+
+Z=X/(np.power(10,8))%10
+Z=m.floor(Z)
+
+print(Z)
+
+#Ir al paso 3+Z 
+
+P=3+Z
+print(P)
+
+if P==4:
+    X=(np.power(X,2)/np.power(10,8))%10
+    print(X)
+    
+if P==5:
+    X=(1001001001X)%10
+    print(X)
+    
+        if(X<100000000):
+            X = X +9814055677
+              else
+            X=X-np.power(10,10)
+
+if(P==6)
+
+
+
+print("Numero de iteraciones")
+
+print("Hellos")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
